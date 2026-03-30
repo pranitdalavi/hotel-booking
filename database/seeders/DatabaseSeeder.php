db6e40c189ac9ca742c47c770071daa166b6c115
Pranit <pranit.dalavi98@gmail.com>
2026-03-30 15:20:36 +0530

    updated databaseseeder file

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,10 +22,12 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        if (!User::where('email', 'test@example.com')->exists()) {
+            User::factory()->create([
+                'name' => 'Test User',
+                'email' => 'test@example.com',
+            ]);
+        }
 
         // ✅ Room Types
         $standard = RoomType::create([
